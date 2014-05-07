37035c1758b0dbace0eba172e4193f14266e411c
ge3k <ge3k@users.noreply.github.com>
2014-05-07 05:51:23 +0530

    Adding few more files before walking the DOM

---
+++ src/main/java/org/ge3k/achilles/DocumentConverter.java
@@ -0,0 +1,12 @@
+package org.ge3k.achilles;
+
+import org.jsoup.nodes.Document;
+
+/**
+ * Created by ge3k on 7/5/14.
+ */
+public class DocumentConverter {
+    public String convert(Document document) {
+        return null;
+    }
+}
--- src/main/java/org/ge3k/achilles/TextifyConverter.java
+++ src/main/java/org/ge3k/achilles/TextifyConverter.java
@@ -4,12 +4,22 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.safety.Cleaner;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by ge3k on 4/5/14.
  */
 public class TextifyConverter {
 
+    private final DocumentConverter documentConverter;
     private Cleaner documentCleaner;
+    private final Lock textConvertLock;
+
+    public TextifyConverter() {
+        textConvertLock = new ReentrantLock();
+        documentConverter = new DocumentConverter();
+    }
 
     public Cleaner getDocumentCleaner() {
         return documentCleaner;
@@ -20,8 +30,14 @@ public class TextifyConverter {
     }
 
     public String convert(String html) {
+        String convertedString = null;
         Document document = Jsoup.parseBodyFragment(html);
         document = documentCleaner.clean(document);
-        //todo continue from here on. stil a rough draft.
+        textConvertLock.lock();
+        try {
+            convertedString = documentConverter.convert(document);
+        } finally {
+           textConvertLock.unlock();
+        }
     }
 }
